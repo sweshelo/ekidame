4bdf49c24c729c2777e89a9bc683f88f1f47d62a
sweshelo <sweshelo@dmats97.xyz>
2021-03-25 21:20:38 +0900

    でんこのスキルのトリガー実装

--- js/app.js
+++ js/app.js
@@ -58,23 +58,47 @@ window.onload = ()=>{
     targ = attack;
     $('#formation_a').addClass("selected");
     $('#formation_b').addClass("unselected");
-    console.log(result);
+    //console.log(result);
 }
 
 
 function getParam(){
-    let init = document.getElementById("init").value;
-    let atk  = document.getElementById("atk").value;
-    let def  = document.getElementById("def").value;
+    let init = Number(document.getElementById("init").value);
+    let atk  = Number(document.getElementById("atk").value);
+    let def  = Number(document.getElementById("def").value);
 
-    let up   = document.getElementById("upper").value;
-    let down = document.getElementById("downer").value;
-    let mio  = document.getElementById("mio").value;
+    let up   = Number(document.getElementById("upper").value);
+    let down = Number(document.getElementById("downer").value);
+    let mio  = Number(document.getElementById("mio").value);
 
     alert(calcDamage(init, atk, def, (up-down), mio, 0));
 }
 
+function sklv(_lv){
+    if(_lv<5){
+        return 0;
+    }else if(_lv<15){
+        return 1;
+    }else if(_lv<30){
+        return 2;
+    }else if(_lv<50){
+        return 3;
+    }else if(_lv<60){
+        return 4;
+    }else if(_lv<70){
+        return 5;
+    }else if(_lv<80){
+        return 6;
+    }else if(_lv==80){
+        return 7;
+    }else{
+        return 0;
+    }
+}
+
 function fillParam(){
+
+    //HP
     if (battle[0]==-1){
         alert('この機能はアタッカーとなるでんこを選択していた場合のみ利用できます。\n対象を選択するには、編成しているでんこのアイコンをクリックします。')
         return -1;
@@ -94,26 +118,80 @@ function fillParam(){
         alert('申し訳ありません。\n指定されたでんこのパラメータ情報は現在存在しません。');
         return -4
     }
-
     document.getElementById('init').value = result[battle[0]-1][level-1];
+
+    let p = [0, 0, 0, 0];
+    let s = false;
+
+    /*
+    for(let i=0;i<attack.length;i++){
+        let sel = document.getElementById('skill_0_'+attack[i]);
+        let l = parseInt(document.getElementById('level_0_'+attack[i]).value);
+        if (sel!=null) {s = Boolean(sel.checked);} else {s=false}
+        console.log(attack[i]);
+        if(sklv(l)&&methods[attack[i]]!=null){
+            let g = methods[attack[i]](sklv(l)-1,s,false);
+            for(let j=0;j<4;j++){p[j]+=Number(g[j])}
+            console.log(i,g);
+        }
+    }
+    for(let i=0;i<block.length;i++){
+        let sel = document.getElementById('skill_1_'+block[i]);
+        let l = parseInt(document.getElementById('level_1_'+block[i]).value);
+        if (sel!=null) {s = Boolean(sel.checked);} else {s=false}
+        if(sklv(l)&&methods[block[i]]!=null){
+            let g = methods[block[i]](sklv(l)-1,s,true);
+            for(let j=0;j<4;j++){p[j]+=Number(g[j])}
+            console.log(i,g);
+            console.log(Number('-1'))
+        }
+    }
+    */
+
+    for(let side=0;side<2;side++){
+        if(side==0){
+            targFromation = attack;
+        }else{
+            targFromation = block;
+        }
+
+        for(let i=0;i<targFromation.length;i++){
+            let sel = document.getElementById('skill_'+side+'_'+targFromation[i]);
+            let l = parseInt(document.getElementById('level_'+side+'_'+targFromation[i]).value);
+            if (sel!=null) {s = Boolean(sel.checked);} else {s=false}
+            console.log(targFromation[i]);
+            if(sklv(l)&&methods[targFromation[i]]!=null){
+                let g = methods[targFromation[i]](sklv(l)-1,s,Boolean(side));
+                for(let j=0;j<4;j++){p[j]+=Number(g[j])}
+                console.log(i,g);
+            }
+        }
+    }
+
+
+    document.getElementById('atk').value    = p[0];
+    document.getElementById('def').value    = p[1];
+    document.getElementById('upper').value  = p[2];
+    document.getElementById('downer').value = p[3];
 }
 
 function calcDamage(_init, _atk, _blk, _const, _mioHP, _elem){
     let damage;
     console.log(_init, _atk, _blk, _const, _mioHP, _elem)
     if (_mioHP > 1){
-        _blk=(100-_blk)/100;
+        _blk=(100+_blk)/100;
         if (_blk<=0) return 1;
         return (_init*(1+_atk/100) - (_mioHP-1))*_blk*(_elem==0 ? 1 : 1.3)+_const;
     }else{
-        console.log(_atk-_blk+100)
-        damage = _init*(_atk-_blk+100)/100*(_elem==0 ? 1 : 1.3)+_const;
+        console.log(_atk+_blk+100)
+        damage = _init*(_atk+_blk+100)/100*(_elem==0 ? 1 : 1.3)+_const;
         return damage;
     }
 }
 
 //編成に追加
 function add(_id){
+    let chkbox = "";
     if (targ.length >= 7){
         console.log("編成は7両以下である必要があります。");
         return -1;
@@ -123,9 +201,12 @@ function add(_id){
         return -2;
     }
     targ.push(_id);
-    targ_e[currentEdit].innerHTML += '<div class="face" ><img src="face/'+_id+'.png" class="select" id="form_'+currentEdit+'_'+_id+'" onclick="select('+currentEdit+','+_id+')"><div class="control"><input type="checkbox" class="skill" id="skill_'+currentEdit+'_'+_id+'"><input type="number" value="80" class="level" id="level_'+currentEdit+'_'+_id+'"></input></div></div>';
+    if (methods[_id]!=null) {chkbox = '<input type="checkbox" class="skill" id="skill_'+currentEdit+'_'+_id+'">'}
+    targ_e[currentEdit].innerHTML += '<div class="face" ><img src="face/'+_id+'.png" class="select" id="form_'+currentEdit+'_'+_id+'" onclick="select('+currentEdit+','+_id+')"><div class="control">'+chkbox+'<input type="number" value="80" class="level" id="level_'+currentEdit+'_'+_id+'"></input></div></div>';
     //$('#form_'+currentEdit+'_'+_id).click(()=>{console.log('clicked'+_id)});
     if (_id === 36 && currentEdit === 1) {$('#mio_row').removeClass('hide');}
+
+    if (battle[currentEdit]==-1) select(currentEdit, _id);
 }
 
 //戦闘対象を選択
