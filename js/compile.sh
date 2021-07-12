3af2d10269f97fb227519e4943c41c8223336ac2
sweshelo <sweshelo@dmats97.xyz>
2021-07-12 15:01:37 +0900

    compile.jsでJSX用の環境を整備できるように

--- js/compile.sh
+++ js/compile.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+if [ -n './node_modules/' ]; then
+
+    if [ ! `node -v | grep -o 'v[0-9\.]*'`] || [ ! `npm -v | grep -o '[0-9\.]*'` ] ;then
+        echo 'Installing nodejs and npm ...'
+        sudo apt update && sudo apt install nodejs npm
+    fi
+
+    echo 'Installing node modules ...'
+    npm init -y
+    npm install babel-cli@6 babel-preset-react-app@3
+fi
+
 npx babel --watch src --out-dir . --presets react-app/prod
