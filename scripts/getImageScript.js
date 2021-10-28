9708bfb7257282d7e327f93bdfec68a9baf5572b
sweshelo <sweshelo@dmats97.xyz>
2021-10-28 23:57:25 +0900

    駅メモ/アワメモ どちらのサーバにも対応

--- scripts/getImageScript.js
+++ scripts/getImageScript.js
@@ -1,15 +1,24 @@
-const parseURL = (dencohName)=>{
-    const version = 'v=2021080401';
-    const wrapping = 'default';
-    const size = 'large';
-    const part = 'face';
-    const emotion = 'smile';
-    return (
-        "https://static.game.our-rails.ekimemo.com/"+version+"/img/partner/"+dencohName+"/"+wrapping+"/"+size+"/"+part+"_"+emotion+".png"
-    )
+const parseURL = (dencohName, isOurMemo)=>{
+    const version = 'v=2021102601';
+
+    let wrapping = 'default';
+    let size = 'large';
+    let part = 'face';
+    let emotion = 'smile';
+
+    let URL = '';
+
+    if (isOurMemo == true){
+        URL = "https://static.game.our-rails.ekimemo.com/"+version+"/img/partner/"+dencohName+"/"+wrapping+"/"+size+"/"+part+"_"+emotion+".png";
+    }else{
+        size = 'small';
+        URL  = "https://static.ekimemo.com/"+version+"/portal/img/common/partner/"+dencohName+"/"+wrapping+"/"+size+"/"+part+"_"+emotion+".png";
+    }
 }
 
 const main = async ()=>{
+
+    const isOurMemo = (document.domain == "static.game.our-rails.ekimemo.com");
 
     await import('https://cdnjs.cloudflare.com/ajax/libs/jszip/3.6.0/jszip.min.js');
     await import('https://cdnjs.cloudflare.com/ajax/libs/jszip-utils/0.1.0/jszip-utils.min.js');
@@ -24,7 +33,7 @@ const main = async ()=>{
         .then(res => res.json())
         .then(res => {
             Promise.all(res.map(dencoh =>(
-                fetch (parseURL(dencoh.name_en)).then(res => {
+                fetch (parseURL(dencoh.name_en, isOurMemo)).then(res => {
                     if(res.ok){
                         zip.file(dencoh.name_en + ".png", res.blob());
                     }
